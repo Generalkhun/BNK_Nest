05bcb0cd396c60902d493c7488e81c4b98b3fd8d
Suttikhun Sihsobhon <suttikhunsihsobhon@Suttikhuns-MacBook-Pro.local>
2020-09-05 16:28:27 +0700

    Add geoJSON district data

--- proj/src/App.js
+++ proj/src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "./App.css";
+import { ShapeFile } from "react-leaflet-shapefile"
 
 function App() {
+
   return (
+    ß
+
     <Map center={[13.7133, 100.5010]} zoom={11}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
