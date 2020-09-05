8cdd2c3f92097e8db229531b30f9f81fc4eb5a05
Pawut Jingjit <pawut.j@gmail.com>
2020-09-05 19:34:17 +0900

    sample

--- proj/src/App.js
+++ proj/src/App.js
@@ -2,20 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "./App.css";
-import { ShapeFile } from "react-leaflet-shapefile"
+import Page2 from "./Page2";
+import data from "./sample.json";
 
 function App() {
-
   return (
-    ß
-
-    <Map center={[13.7133, 100.5010]} zoom={11}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </Map>
-
+    <div>
+      <Page2 />
+      <button
+        onClick={() => {
+          console.log(data);
+        }}
+      >
+        CheckData
+      </button>
+    </div>
   );
 }
 
