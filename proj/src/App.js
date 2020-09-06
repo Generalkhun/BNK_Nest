086dd9e0283fb4c856f8f6d7392f01cb91e2cf25
Suttikhun Sihsobhon <suttikhunsihsobhon@Suttikhuns-MacBook-Pro.local>
2020-09-06 11:36:20 +0700

    add new district polygons

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Map, Marker, Popup, TileLayer, Polygon } from "react-leaflet";
 import "./App.css";
+//import district shape data
+import * as districtShape from "./data/district/districtNew.json";
+// import { Polygon } from "leaflet";
 
 function App() {
+  //console.log(districtShape.features[0].geometry.coordinates[0])
+  let [districtSelected, setDistrictSelected] = useState("");
 
   return (
-    
-
-    <Map center={[13.7133, 100.5010]} zoom={11}>
+    <Map center={[13.7133, 100.501]} zoom={11}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-    </Map>
 
+      {districtShape.features.map((district) => {
+        let thisPositionArray = district.geometry.coordinates[0];
+
+        //Adjust latitude and longtitude position inside the array
+        thisPositionArray.map((thisPositionPoint) => {
+          if (thisPositionPoint[1] < thisPositionPoint[0]) {
+            const sp = thisPositionPoint[1];
+            thisPositionPoint[1] = thisPositionPoint[0];
+            thisPositionPoint[0] = sp;
+          }
+        });
+        return (
+          <Polygon
+            key={district.properties.OBJECTID}
+            positions={thisPositionArray}
+            onClick={() => {
+              console.log(district.properties.dname);
+              setDistrictSelected(district.properties.dname);
+            }}
+          />
+        );
+      })}
+    </Map>
   );
 }
 
