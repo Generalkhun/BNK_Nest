507ed6da539f65dfabef60b500bc3e62c64b56ae
Suttikhun Sihsobhon <suttikhunsihsobhon@Suttikhuns-MacBook-Pro.local>
2020-09-06 11:34:45 +0700

    add polygon of district, clicking can return district name on statevariable

--- proj/src/App.js
+++ proj/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "./App.css";
-import { ShapeFile } from "react-leaflet-shapefile"
 
 function App() {
 
