a0c5aaea18a554506bcf308c682466a3f0e5d6d5
Suttikhun Sihsobhon <suttikhunsihsobhon@Suttikhuns-MacBook-Pro.local>
2020-09-07 13:54:18 +0700

    start creating choropleth

--- proj/src/MapDistrict.js
+++ proj/src/MapDistrict.js
@@ -1,55 +1,60 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import { Map, Marker, Popup, TileLayer, Polygon } from "react-leaflet";
+import { Map, TileLayer, Polygon } from "react-leaflet";
 import "./App.css";
+
+import Choropleth from "react-leaflet-choropleth";
+
 //import district shape data
 import * as districtShape from "./data/district/districtNew.json";
-// import { Polygon } from "leaflet";
+import * as districtShapeGEOJSON from "./data/district/districtNew.geojson";
+
+//import district expense data to display fill Visualization
+import * as districtExpenseData from "./sample.json";
 
 function MapDistrict() {
-  //console.log(districtShape.features[0].geometry.coordinates[0])
+  console.log(console.log(districtExpenseData[0]));
   let [districtSelected, setDistrictSelected] = useState("");
   let [mouseOnDistrict, setmouseOnDistrict] = useState("");
 
+  // style of choropleth
+  const style = {
+    weight: 2,
+    opacity: 1,
+    color: "white",
+    dashArray: "3",
+    fillOpacity: 0.6,
+  };
+
   return (
     <Map center={[13.7133, 100.501]} zoom={11}>
-      <TileLayer
+          <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
 
-      {districtShape.features.map((district) => {
-        let thisPositionArray = district.geometry.coordinates[0];
-
-        //Adjust latitude and longtitude position inside the array
-        thisPositionArray.map((thisPositionPoint) => {
-          if (thisPositionPoint[1] < thisPositionPoint[0]) {
-            const sp = thisPositionPoint[1];
-            thisPositionPoint[1] = thisPositionPoint[0];
-            thisPositionPoint[0] = sp;
-          }
-        });
-        return (
-          <Polygon
-            key={district.properties.OBJECTID}
-            positions={thisPositionArray}
-            onClick={() => {
-              console.log(district.properties.dname);
-              setDistrictSelected(district.properties.dname);
-            }}
-            fillColor={
-              district.properties.dname === mouseOnDistrict ? "Orange" : "Green"
-            }
-            color={
-              district.properties.dname === mouseOnDistrict ? "White" : "Grey"
-            }
-            weight="1"
-            onMouseOver={() => {
-              setmouseOnDistrict(district.properties.dname);
-            }}
-          />
-        );
-      })}
+      <Choropleth
+        data={{ type: "FeatureCollection", features: districtShape.features }}
+        identity = {(feature) => feature.properties.OBJECTID}
+        valueProperty={(feature) => feature.properties.no_female}
+        visible={feature => true}
+        scale={[
+          "#FDE725FF",
+          "#73D055FF",
+          "#29AF7FFF",
+          "#238A8DFF",
+          "#39568CFF",
+          "#482677FF",
+          "#440154FF",
+        ]}
+        steps={7}
+        mode="e"
+        style={style}
+        onEachFeature={(feature, layer) =>
+          layer.bindPopup(feature.properties.dname)
+        }
+        //ref={(el) => (this.Choropleth = el.leafletElement)}
+      />
     </Map>
   );
 }
