c25cba5f4ebfeef7777cbdd3dc9b60d4d37a5f0f
Generalkhun <khun2551@hotmail.com>
2020-09-09 17:19:20 +0700

    dc

--- proj/src/Page2.js
+++ proj/src/Page2.js
@@ -56,6 +56,7 @@ function Page2({ districtSelected }) {
   const [filter, setFilter] = useState(1);
 
   return (
+    <div style = {{padding:20}}>
     <div style={{ fontFamily: `'Kanit', sans-serif`, color: "white" }}>
       <div>
         <h1>
@@ -63,12 +64,10 @@ function Page2({ districtSelected }) {
             ? districtNametoDistrict(districtSelected)[0].name
             : "null"}
         </h1>
-
-        <Option setFilter={setFilter} />
       </div>
       <div
         style={{
-          gridTemplateColumns: `1fr 1fr 1fr`,
+          gridTemplateColumns: `2fr 1fr 1fr`,
           gridTemplateRows: `1fr 1fr 1fr`,
           display: "grid",
           gridTemplateAreas: `
@@ -77,35 +76,28 @@ function Page2({ districtSelected }) {
                     'main7 main7 main7'
                 `,
           height: 300,
-          width: 800,
+          width: 460,
         }}
       >
-        <div style={{ gridArea: "main1" }}></div>
-        <div style={{ gridArea: "main2", margin: "auto" }}>
-          <p>งบปีปัจจุบัน</p>
-          <p>
+        <div style={{ gridArea: "main1" }}>
+        <p>รวมงบประมานที่ได้รับปี 63</p>
+          <h2>
             {districtNametoDistrict(districtSelected).length > 0
               ? districtNametoDistrict(districtSelected)[0][`f${filter}_sum`]
               : 0}{" "}
             บาท
-          </p>
+          </h2>
+        </div>
+        <div style={{ gridArea: "main2", margin: "auto" }}>
+          
         </div>
         <div style={{ gridArea: "main3" }}></div>
         <div style={{ gridArea: "main4", margin: "auto" }}>
-          {districtNametoDistrict(districtSelected).length > 0
-            ? districtNametoDistrict(districtSelected)[0]
-                [`f${filter}_data_f`].replace(/_$/g, "")
-                .split("_")
-                .map((s, i) => (
-                  <p>
-                    {i}.{s} บาท
-                  </p>
-                ))
-            : ""}
+
         </div>
         <div style={{ gridArea: "main5" }}></div>
         <div style={{ gridArea: "main6" }}></div>
-        <div style={{ gridArea: "main7" }}>
+        <div style={{ gridArea: "main7", paddingTop:30  }}>
           {districtNametoDistrict(districtSelected).length > 0 && (
             <DonutChart
               data={dataToPieObject(
@@ -121,6 +113,9 @@ function Page2({ districtSelected }) {
         </div>
       </div>
     </div>
+
+    </div>
+    
   );
 }
 
