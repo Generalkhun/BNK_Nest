b3c16a7e5ea2fcb18a1a973c93399c84f83a8d16
Generalkhun <khun2551@hotmail.com>
2020-09-12 23:36:12 +0700

    Add form

--- proj/src/LeftSideForum.js
+++ proj/src/LeftSideForum.js
@@ -2,15 +2,38 @@ import React from "react";
 import Profile from "./Profile.js";
 import { Form, Button } from "semantic-ui-react";
 
-
 function LeftSideForum() {
   return (
     <div>
       <Profile />
-      <Form reply>
-        <Form.TextArea />
-        <Button content="Add Reply" labelPosition="left" icon="edit" primary />
-      </Form>
+      <Form.Field>
+        <div style={{ paddingLeft: 1 }}>
+          <p>ชื่อที่ใช้แสดงความเห็น</p>
+        </div>
+        <input placeholder="ชื่อ" />
+      </Form.Field>
+
+      <Form.Field>
+        <div style={{ paddingLeft: 1 }}>
+          <p>หัวข้อ</p>
+        </div>
+        <input placeholder="เขียนหัวข้อที่นี่..." />
+      </Form.Field>
+
+      <Form.Field>
+        <div style={{ paddingLeft: 1 }}>
+          <p>เนิ้อหา</p>
+        </div>
+        <Form reply>
+          <Form.TextArea />
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+          />
+        </Form>
+      </Form.Field>
     </div>
   );
 }
